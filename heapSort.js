3409fc7e9f13a978d3023fdd67ae71aed482376b
someramid <71240082+someramid@users.noreply.github.com>
2021-05-14 12:17:41 -0700

    Update heapSort.js

--- heapSort.js
+++ heapSort.js
@@ -30,14 +30,40 @@ function insert(a, num){
 
 let a = [undefined, 9, 8, 7, 6, 7, 2, 6, 5, 1];
 
-insert(a, 20); 
+// insert(a, 20); 
 
-a = [undefined, 9, 8, 7, 6, 7, 2, 6, 5, 1];
-insert(a, 0); 
+// a = [undefined, 9, 8, 7, 6, 7, 2, 6, 5, 1];
+// insert(a, 0); 
 
-insert([], 20);
+// insert([], 20);
 
-insert([0], 20);
+// insert([0], 20);
+
+
+function findIdx(a, num) {
+  const len = a.length;
+  if (len == 0) {
+    return;
+  }
+  let i = 0;
+  
+  for (i=0; i<len; i++) {
+    if (a[i] == num) {
+      return i;
+    } else if(a[i] < num) {
+      break;
+    }
+  }  
+}
+
+let b = [undefined, 9, 8, 7, 6, 7, 2, 6, 5, 1];
+
+console.log(findIdx(b, 20)); //number not in array
+console.log(findIdx(b, 9)); //root node
+console.log(findIdx(b, 1)); //last element
+console.log(findIdx([], 1)); //empty array
+console.log(findIdx([undefined, 1], 1)); //one node
+console.log(findIdx([undefined, 5], 1)); //one node, not found
 
 
 
