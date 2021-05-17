c46b6fb2b4cc910d1caa696742f858b90ec8b74f
someramid <71240082+someramid@users.noreply.github.com>
2021-05-17 00:30:22 -0700

    Update arrayElementsAddToK.js

--- arrayElementsAddToK.js
+++ arrayElementsAddToK.js
@@ -3,13 +3,15 @@
   
   AND 
   
-  IMPORTANT GOTCHA: I was sending prefixArr and remArr in recursion calls but as they were getting manipulated like push and pop, logic in some other recursion calls was not working as expected like remArr.length was 0 when it shouldn't be. So, copies of arrays should be sent in recursions.
+  IMPORTANT GOTCHA: I was sending prefixArr and remArr in recursion calls but as they were getting manipulated like push and pop, 
+  logic in some other recursion calls was not working as expected like remArr.length was 0 when it shouldn't be. 
+  So, copies of arrays should be sent in recursions.
 
 */
 
 
 
-// Find all sets of array elemnts adding to sum
+// Find all sets of array elemnts adding to sum. Further below is function just to print all sub sets
 function recursionForSum(arr, prefixArr, remArr, sumSoFar, sum) {
   //Break
   if(sumSoFar === sum) {
@@ -48,7 +50,8 @@ function recursion(arr, prefixArr, remArr) {
   //Decrease 
   const curEle = remArr.pop();
 
-  //IMPORTANT GOTCHA: Send array copies, otherwise as they are updated, some other recusion call not working as expected because these 2 arrays are changing outside of the current recursion call
+  //IMPORTANT GOTCHA: Send array copies, otherwise as they are updated, some other recusion call not working as expected because these 2 arrays
+  // are changing outside of the current recursion call
   recursion(arr, [...prefixArr], [...remArr]);
   recursion(arr, [...prefixArr, curEle], [...remArr]);
 }
